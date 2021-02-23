bf8ca9089ea3bb075830b663555612c9b26e7cf6
AlexKmetyuk <kmetyuk.sasha@gmail.com>
2021-02-23 15:51:57 +0200

    debug load more btn

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import refs from './js/refs';
 import fetchImages from './js/apiService';
 import renderGallery from './js/renderGallery'
 
+let page = 0;
+
+
 refs.btnSubmit.addEventListener('click', event => {
     refs.gallery.innerHTML=''
     event.preventDefault()
+    page = 1
     fetchImages(refs.input.value)
-        .then(images => renderGallery(images))
-    refs.loadMoreBtn.classList.toggle("is-hidden")
-})
-
-let page = 1
+        .then(images => renderGallery(images))  
+    if (page > 1) {
+            refs.loadMoreBtn.classList.add("is-hidden")
+        } else refs.loadMoreBtn.classList.remove("is-hidden")
+    })
 
 refs.loadMoreBtn.addEventListener('click', event => {
     event.preventDefault()
@@ -19,4 +23,5 @@ refs.loadMoreBtn.addEventListener('click', event => {
     fetchImages(refs.input.value, page)
         .then(images => renderGallery(images))
 })
+
 
