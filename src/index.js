acda34b2d9311f6df2eb53aef1f999035c766f97
AlexKmetyuk <kmetyuk.sasha@gmail.com>
2021-03-03 20:02:08 +0200

    added pnotify and normalize

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import './styles.css';
+
+import { error } from "@pnotify/core";
+import "@pnotify/core/dist/PNotify.css";
+import "@pnotify/core/dist/BrightTheme.css";
+import * as Confirm from "@pnotify/confirm";
+import "@pnotify/confirm/dist/PNotifyConfirm.css";
+
 import refs from './js/refs';
 import fetchImages from './js/apiService';
 import renderGallery from './js/renderGallery'
@@ -9,19 +16,37 @@ let page = 0;
 refs.btnSubmit.addEventListener('click', event => {
     refs.gallery.innerHTML=''
     event.preventDefault()
-    page = 1
     fetchImages(refs.input.value)
-        .then(images => renderGallery(images))  
-    if (page > 1) {
-            refs.loadMoreBtn.classList.add("is-hidden")
-        } else refs.loadMoreBtn.classList.remove("is-hidden")
-    })
+        .then(images => {
+            renderGallery(images)
+            if (images.length > 0) {
+                showLoadBtn()
+            } else {
+                hideLoadBtn()
+                error({
+                    text: 'Nothing was found! Еnter the correct query!',
+                });
+            }
+        })
+})
 
 refs.loadMoreBtn.addEventListener('click', event => {
     event.preventDefault()
     page++
     fetchImages(refs.input.value, page)
-        .then(images => renderGallery(images))
+        .then(images => {
+            renderGallery(images)
+            scrollTo({
+                top: document.documentElement.offsetHeight,
+                behavior: 'smooth'
+            })
+        })
 })
 
+function showLoadBtn() {
+    refs.loadMoreBtn.classList.remove("is-hidden")
+}
 
+function hideLoadBtn() {
+    refs.loadMoreBtn.classList.add("is-hidden")
+}
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,4 +1,5 @@
 const key = '20385561-6a350e58d651a37e998e9aef4'
+
 function fetchImages(value, page) {
     return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${value}&page=${page}&per_page=12&key=${key}`)
         .then(res => res.json())
